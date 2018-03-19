d361497c3d00277b2fde908f64f92bdf33f1b30b
Alpha <los8won@gmail.com>
2018-03-19 09:52:46 +0800

    搭建前台系统

---
+++ portal/src/main/java/com/itheima/bos/fore/web/action/CustomerAction.java
@@ -0,0 +1,10 @@
+package com.itheima.bos.fore.web.action;  
+/**  
+ * ClassName:CustomerAction <br/>  
+ * Function:  <br/>  
+ * Date:     2018年3月19日 上午9:49:34 <br/>       
+ */
+public class CustomerAction {
+
+}
+  
